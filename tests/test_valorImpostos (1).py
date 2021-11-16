4f400b448523f1964ad5bfde640e73337ebdd2a7
RonaldoThierre <69646493+RonaldoThierre@users.noreply.github.com>
2021-11-16 20:40:21 -0300

    Teste_Imposto
    Teste feito.

---
+++ tests/test_valorImpostos (1).py
@@ -0,0 +1,23 @@
+from unittest import TestCase, mock
+from app.controler.patrimonio import PatrimonioControler
+
+
+class PatrimonioValor(TestCase):
+
+
+
+    @mock.patch('app.controler.patrimonio.valor_icms')
+    def test_Mockado_impostos_ICMS(self,mokodoValue):
+        mokodoValue.return_value = 3
+        assert PatrimonioControler.CalculaValorPatrimonio(100,10,"",5) == 118
+    
+    @mock.patch('app.controler.patrimonio.valor_ipi')
+    def test_Mockado_impostos_IPI(self,mokodoValue):
+        mokodoValue.return_value = 90
+        assert PatrimonioControler.CalculaValorPatrimonio(100,10,10,"") == 210
+
+    @mock.patch('app.controler.patrimonio.valor_frete')
+    def test_Mockado_frete(self,mokodoValue):
+        mokodoValue.return_value = 100
+        assert PatrimonioControler.CalculaValorPatrimonio(100,"",10,5) == 115  
+
